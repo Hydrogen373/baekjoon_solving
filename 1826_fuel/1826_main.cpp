5e6434394b04e586be31c1bdca3e3b71a13c5988
watari <shyun2314@naver.com>
2024-02-03 02:00:32 +0900

    clear

--- 1826_fuel/1826_main.cpp
+++ 1826_fuel/1826_main.cpp
@@ -1,23 +1,57 @@
 #include<iostream>
 #include<algorithm>
+#include<cassert>
 
-using std::make_heap;
+using namespace std;
+using Station = pair<int, int>;
 
 int N;
-int L, P;
+unsigned long long int L, P;
+unsigned long long int fuel;
+Station* stations;
+int arrSize;
+int maxInd;
+int result{ 0 };
+bool comp(const Station& small, const Station& big) {
+	if ((small.first <= fuel) == (big.first <= fuel))
+		return small.second < big.second;
+	else return small.first > fuel;
+}
+
 int main() {
 	using namespace std;
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 
 	cin >> N;
+	stations = new Station[N];
+	arrSize = N;
 	for (int i = 0; i < N; i++) {
 		int a, b;
 		cin >> a >> b;
 
+		stations[i] = make_pair(a, b);
 	}
 
+	cin >> L >> P;
+	fuel = P;
 
+	make_heap(stations, stations + arrSize, comp);
 
-	
+	while (fuel < L) {
+		if (stations[0].first > fuel || arrSize <= 0) {
+			result = -1;
+			break;
+		}
+
+		fuel += stations[0].second;
+		pop_heap(stations, stations + arrSize, comp);
+		arrSize--;
+		result++;
+		make_heap(stations, stations + arrSize, comp);
+	}
+	cout << result;
+
+	delete[] stations;
+	return 0;
 }
