561ebf1cbb59e71964924f59e07b6cdfd33b90fd
Hydrogen373 <shyun2314@naver.com>
2024-02-02 19:21:53 +0900

    start 1826

---
+++ 1826_fuel/1826_main.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<algorithm>
+
+using std::make_heap;
+
+int N;
+int L, P;
+int main() {
+	using namespace std;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		int a, b;
+		cin >> a >> b;
+
+	}
+
+
+
+	
+}
