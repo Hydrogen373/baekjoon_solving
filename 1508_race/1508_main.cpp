e4c229d4cdd655ec237d7c48a721dede95c90929
Hydrogen373 <shyun2314@naver.com>
2024-02-06 21:22:56 +0900

    clear

--- 1508_race/1508_main.cpp
+++ 1508_race/1508_main.cpp
@@ -1,80 +1,55 @@
 #include<iostream>
-#include<map>
-#include<algorithm>
 
 int N, M, K;
-std::map<int, int> gaps;
+int* locs;
 
-struct compSecond {
-	bool operator() (const std::pair<int, int>& a, const std::pair<int, int>& b) {
-		int gapA = a.second - a.first;
-		int gapB = b.second - b.first;
-		return gapA < gapB || gapA == gapB && a.first > b.first;
-
+int count(int gp) {
+	int lastInd = 0;
+	int result = 1;
+	for (int i = 1; i < K; i++) {
+		if (locs[i] - locs[lastInd] >= gp) {
+			lastInd = i;
+			result++;
+		}
 	}
-};
+	return result;
+}
 
 int main() {
-	std::ios::sync_with_stdio(false);
-	std::cin.tie(0);
-
 	std::cin >> N >> M >> K;
 
-	int* locs = new int[K];
+	locs = new int[K];
 	for (int i = 0; i < K; i++) {
 		std::cin >> locs[i];
 	}
-	for (int i = 0; i < K - 1; i++) {
-		gaps.insert(std::make_pair(locs[i], locs[i + 1]));
-	}
 
-	for (int i = 0; i < K - M; i++) {
-		auto iter = std::min_element(gaps.begin(), gaps.end(), compSecond());
-
-		auto lg = iter;
-		auto rg = iter;
-		if (iter != gaps.begin() && iter != --(gaps.end())) {
-			auto tmp = iter;
-			auto ttmp = iter;
-			tmp--; ttmp++;
-			if (compSecond()(*tmp, *ttmp)) {
-				lg--;
-			}
-			else {
-				rg++;
-			}
-		}
-		else if (iter == gaps.begin()) {
-			rg++;
+	int low = 0;
+	int high = N;
+	int mid = (high + low) / 2;
+	while (high >= low) {
+		mid = (high + low) / 2;
+		int ct = count(mid);
+		if (ct < M) {
+			high = mid - 1;
 		}
 		else {
-			lg--;
+			low = mid + 1;
 		}
-
-		lg->second = rg->second;
-		gaps.erase(rg);
 	}
-
-	for (auto iter = gaps.begin(); iter != gaps.end(); iter++) {
-		std::cout << iter->first << ' ' << iter->second << '\n';
-	}
-
-	auto iter = std::min_element(gaps.begin(), gaps.end(), compSecond());
-
-	int gp = iter->second - iter->first;
-
-	int counter = 1;
-	int last = locs[0];
+	
+	int last = 0;
+	int ct = 1;
 	std::cout << 1;
 	for (int i = 1; i < K; i++) {
-		if (locs[i] - last >= gp && counter < M) {
+		if (locs[i] - locs[last] >= high && ct < M) {
 			std::cout << 1;
-			last = locs[i];
-			counter++;
+			last = i;
+			ct++;
 		}
-		else
+		else {
 			std::cout << 0;
-
+		}
 	}
 	delete[] locs;
+
 }
