1c57215720dfc54d739754d3bababa61e248a118
Hydrogen373 <shyun2314@naver.com>
2024-02-11 20:05:52 +0900

    checkpoint

--- 31399_aris/main.cpp
+++ 31399_aris/main.cpp
@@ -6,34 +6,67 @@ int R, C, D;
 
 int dr[]{ -1,0,1,0 };
 int dc[]{ 0,1,0,-1 };
-char A[1024][1024];
-char B[1024][1024];
-std::tuple<int, int, int> loopPoint;
-bool looping = false;
-bool dust[1024][1024];
 
-void rotate(char c) {
-	D = (D + c) % 4;
-}
-bool loopTest() {
-	return looping && std::make_tuple(R, C, D) == loopPoint;
-}
-void setLoop() {
-	looping = true;
-	loopPoint = std::make_tuple(R, C, D);
-}
+char** A;
+char** B;
 
-unsigned long long int moveCount = 0;
-void move(bool count) {
-	static unsigned long long int buf = 0;
-	R += dr[D];
-	C += dc[D];
-	if (count) {
-		moveCount += buf + 1;
-		buf = 0;
+struct Status {
+	unsigned short r, c;
+	unsigned char d;
+	unsigned int distance;
+	Status(unsigned short r, unsigned short c, char d, unsigned int distance) :
+		r(r), c(c), d(d), distance(distance) {}
+	Status() :Status(0, 0, 0, 0) {}
+	void rotate(char c) {
+		d = (d + c) % 4;
+	}
+	void move() {
+		r += dr[d];
+		c += dc[d];
+		distance += 1;
+	}
+	bool operator==(const Status& other) {
+		return r == other.r && c == other.c && d == other.d;
+	}
+	void jump(const Status& after) {
+		r = after.r;
+		c = after.c;
+		d = after.d;
+		distance += after.distance;
+	}
+};
+Status*** jumpTable;
+Status aris;
+Status endPoint(0xffff, 0xffff, 0xff, 0);
+Status find(const Status& s) {
+	if (jumpTable[s.r][s.c][s.d] == s || jumpTable[s.r][s.c][s.d] == endPoint) {
+		return jumpTable[s.r][s.c][s.d];
 	}
 	else {
-		buf++;
+		//unsigned int tmp = jumpTable[s.r][s.c][s.d].distance;
+		//jumpTable[s.r][s.c][s.d] = find(jumpTable[s.r][s.c][s.d]);
+		//jumpTable[s.r][s.c][s.d].distance += tmp;
+		jumpTable[s.r][s.c][s.d].jump(find(jumpTable[s.r][s.c][s.d]));
+		return jumpTable[s.r][s.c][s.d];
+	}
+}
+void initJumpTable(const Status& a) {
+	if (jumpTable[a.r][a.c][a.d].distance != 0)
+		return;
+	auto r = a.r;
+	auto c = a.c;
+
+	for (int i = 0; i < 4; i++) {
+		Status tmp = Status(r, c, i, 0);
+		tmp.rotate(B[r][c]);
+		tmp.move();
+		if (tmp.r < 0 || tmp.r >= H || tmp.c < 0 || tmp.c >= W || find(tmp) == a) {
+			tmp.jump(endPoint);
+		}
+		else {
+			tmp.jump(find(tmp));
+		}
+		jumpTable[r][c][i] = tmp;
 	}
 }
 
@@ -42,54 +75,68 @@ int main() {
 	std::cin.tie(0);
 
 	std::cin >> H >> W;
+
 	std::cin >> R >> C >> D;
+	aris = Status(R, C, D, 0);
 
-	setLoop();
-
-	int dustCt = H * W;
+	A = new char* [H];
 	for (int i = 0; i < H; i++) {
+		A[i] = new char[W];
 		for (int j = 0; j < W; j++) {
 			std::cin >> A[i][j];
+			A[i][j] -= '0';
 		}
 	}
+	B = new char* [H];
 	for (int i = 0; i < H; i++) {
+		B[i] = new char[W];
 		for (int j = 0; j < W; j++) {
 			std::cin >> B[i][j];
-		}
-	}
-	for (int i = 0; i < H; i++) {
-		for (int j = 0; j < W; j++) {
-			dust[i][j] = true;
+			B[i][j] -= '0';
 		}
 	}
 
-	while (dustCt >= 0) {
-		if (dust[R][C]) {
-			dust[R][C] = false;
-			dustCt--;
-			if (A[R][C] != B[R][C])
-				looping = false;
-			else if (looping == false) {
-				setLoop();
+	jumpTable = new Status * *[H];
+	for (int i = 0; i < H; i++) {
+		jumpTable[i] = new Status * [W];
+		for (int j = 0; j < W; j++) {
+			jumpTable[i][j] = new Status[4];
+			for (int k = 0; k < 4; k++) {
+				jumpTable[i][j][k] = Status(i, j, k, 0);
 			}
-			rotate(A[R][C]);
-			move(true);
+		}
+	}
+
+	while (true) {
+		auto next = find(aris);
+		if (next == endPoint)
+			break;
+
+		if (next == aris) {
+			initJumpTable(next);
+			aris.rotate(A[aris.r][aris.c]);
+			aris.move();
 		}
 		else {
-			if (looping == false) {
-				setLoop();
-			}
-			rotate(B[R][C]);
-			move(false);
+			aris.jump(next);
 		}
 
-		if (R < 0 || R >= H || C < 0 || C >= W)
-			break;
-
-		if (loopTest() == true)
+		if (aris.r < 0 || aris.r >= H || aris.c < 0 || aris.c >= W)
 			break;
 	}
-	std::cout << moveCount;
+	std::cout << aris.distance;
+
+	for (int i = 0; i < H; i++) {
+		delete[] A[i], B[i];
+	}
+	delete[] A, B;
+	for (int i = 0; i < H; i++) {
+		for (int j = 0; j < W; j++) {
+			delete[] jumpTable[i][j];
+		}
+		delete[] jumpTable[i];
+	}
+	delete[] jumpTable;
 
 	return 0;
 }
