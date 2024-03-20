b722bf9007b14dbd4ef394192785ff3732b9b4a5
watari <shyun2314@naver.com>
2024-03-21 03:50:24 +0900

    clear 2143 sum of sequence

--- 2143_sum_of_sequence/2143_sum_of_sequence.cpp
+++ 2143_sum_of_sequence/2143_sum_of_sequence.cpp
@@ -1,9 +1,43 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 std::vector<int> sumA, sumB;
 int T;
 int n;
 int m;
+
+void init(std::vector<int>& vec) {
+	int L = vec.size();
+	for (int i = 0; i <= L - 2; i++) {
+		int v = vec[i];
+		for (int j = i + 1; j <= L - 1; j++) {
+			v += vec[j];
+			vec.push_back(v);
+		}
+	}
+	std::sort(vec.begin(), vec.end());
+	
+	//// debug
+	//for (auto iter : vec) {
+	//	std::cout << iter << ' ';
+	//}
+	//std::cout << std::endl;
+}
+
+int bisearch(const std::vector<int>& vec, int value) {
+	int r = vec.size();
+	int l = 0;
+	while (l < r) {
+		int mid = (l + r) / 2;
+		if (vec[mid] >= value) {
+			r = mid;
+		}
+		else {
+			l = mid + 1;
+		}
+	}
+	return r;
+}
 
 int main() {
 	std::ios::sync_with_stdio(0);
@@ -22,15 +56,23 @@ int main() {
 		sumB.push_back(b);
 	}
 	//
-	int l, r;
-	int nr = 0;
-	for (int l= 0; l< n; l++) {
-		
+	init(sumA);
+	init(sumB);
 
+	int a = 0;
+	unsigned long long res = 0;
+	while (a < sumA.size()) {
+		int v = sumA[a];
+		int n = bisearch(sumA, v + 1);
+		unsigned long long c1 = n - a;
+		a = n;
 
+		unsigned long long c2 = bisearch(sumB, T - v + 1) - bisearch(sumB, T - v);
+
+		res += c1 * c2;
 	}
 
-
+	std::cout << res;
 	//
 	return 0;
 
